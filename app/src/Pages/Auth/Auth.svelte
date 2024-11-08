66a1afb61e1169ceae06dc229381ccb14c4fee1d
theseyan <sayanjyotidasworkmail@gmai.com>
2024-11-08 16:13:45 +0530

    Basic auth page (sayan)

---
+++ app/src/Pages/Auth/Auth.svelte
@@ -0,0 +1,52 @@
+<script>
+    import * as Card from "$ui/components/ui/card";
+    import {onMount} from 'svelte';
+    import {Auth} from '../../lib';
+    import {replace} from 'svelte-spa-router';
+
+    onMount(() => {
+        document.title = "Sign In — WellWave"
+    });
+
+    window.onGISCallback = (data) => {
+        Auth.signInOrCreateAccount(data.credential).then(async () => {
+            replace('/');
+        }).catch(e => {
+            console.error("Failed to sign in:", e);
+            alert("There was an error while signing in. Please try again.");
+        });
+    };
+</script>
+
+<svelte:head>
+    <script src="https://accounts.google.com/gsi/client" async></script>
+</svelte:head>
+
+<div class="auth">
+    <div class="logo"></div>
+    
+    <Card.Root>
+        <Card.Header>
+            <Card.Title>Sign In</Card.Title>
+            <Card.Description>Mental health for a better future.</Card.Description>
+        </Card.Header>
+        <Card.Content class="text-center">
+            <div id="g_id_onload"
+                data-client_id="941623673011-ght4tnsqh9n60hhpf7lqaaoc25ct7ac9.apps.googleusercontent.com"
+                data-context="signin"
+                data-ux_mode="popup"
+                data-callback="onGISCallback"
+                data-auto_prompt="false">
+            </div>
+
+            <div class="g_id_signin"
+                data-type="standard"
+                data-shape="rectangular"
+                data-theme="outline"
+                data-text="signin_with"
+                data-size="large"
+                data-logo_alignment="left">
+            </div>
+        </Card.Content>
+    </Card.Root>
+</div>
--- app/src/Pages/index.js
+++ app/src/Pages/index.js
@@ -6,3 +6,4 @@ export {default as Home} from './Home/Home.svelte';
 export {default as MyDay} from './MyDay/MyDay.svelte';
 export {default as NotFound} from './NotFound/NotFound.svelte';
 export {default as About} from './About/About.svelte';
+export {default as Auth} from './Auth/Auth.svelte';
